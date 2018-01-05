0edd325db8ee33f13bd58fe8ae09c47ea3e35e93
alvivar <andresalvivar@gmail.com>
2018-01-05 15:20:30 -0600

    Scrapping user page from Twitch wip

--- twitchscrapper.py
+++ twitchscrapper.py
@@ -13,28 +13,34 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-def selenium_get_html(url, chrome_driver, language="en"):
+def selenium_get_html(url, chrome_driver, language=None):
     """ Return the html source from the url using Selenium and the Chrome web
-    driver. """
+    driver. If language is specified (es, en, fr, etc) it will click on the
+    language menu and change it (works only on directory pages). """
 
     driver = webdriver.Chrome(chrome_driver)
     driver.get(url)
 
-    driver.find_element_by_xpath(
-        "//div[@class='language-select-menu ']").click()
-    driver.find_element_by_xpath(
-        f"//div[@class='tw-checkbox' and @data-language-code='{language}']"
-    ).click()
-    time.sleep(1)
+    # Change the menu language by 'clicking'
+    if language:
+        driver.find_element_by_xpath(
+            "//div[contains(@class, 'language-select-menu')]").click()
+        time.sleep(1)
+
+        driver.find_element_by_xpath(
+            f"//div[@class='tw-checkbox' and @data-language-code='{language}']"
+        ).click()
+        time.sleep(1)
 
     html = driver.page_source
-
     driver.quit()
 
     return html
 
 
 def get_directory_data(htmlsource):
+    """ Return a dictionary with the data for each stream in a Twitch game
+    directory page, like https://www.twitch.tv/directory/game/Overwatch """
 
     soup = BeautifulSoup(htmlsource, "html.parser")
 
@@ -64,6 +70,51 @@ def get_directory_data(htmlsource):
     return data
 
 
+def get_user_data(htmlsource):
+    """ Return a dictionary with the user data on a Twitch.tv streamer page html
+    source, like https://www.twitch.tv/chipshajen """
+
+    soup = BeautifulSoup(htmlsource, "html.parser")
+
+    data = {}
+
+    status = soup.find("span", {
+        "data-a-target": "stream-title",
+        "title": True
+    })
+    status = status["title"] if status else None
+
+    viewers = soup.find("div", {
+        "class": "tw-stat",
+        "data-a-target": "channel-viewers-count"
+    }).find("span", {
+        "data-a-target": "tw-stat-value"
+    })
+    viewers = viewers.text if viewers else None
+
+    total_views = soup.find("div", {
+        "class": "tw-stat",
+        "data-a-target": "total-views-count"
+    }).find("span", {
+        "data-a-target": "tw-stat-value"
+    })
+    total_views = total_views.text if total_views else None
+
+    data["userx"] = {
+        "status": status,
+        "twitter": "",
+        "instagram": "",
+        "viewers": viewers,
+        "total_views": total_views,
+        "followers": "",
+        "following": "",
+        "videos_count": "",
+        "tags": "",
+    }
+
+    return data
+
+
 if __name__ == "__main__":
 
     DELTA = time.time()
@@ -86,32 +137,38 @@ if __name__ == "__main__":
         with open(CONFIGJSON, "w") as f:
             json.dump(CONFIG, f)
 
-    # Drivers
+    # Driver
 
     CHROME = os.path.normpath(
         os.path.join(CDIR, CONFIG["config"]["chrome_driver_path"]))
 
     # Data
 
-    HTMLES = selenium_get_html(
-        "https://www.twitch.tv/directory/game/Overwatch",
-        CHROME,
-        language="es")
-    DATAES = get_directory_data(HTMLES)
+    # DATAES = get_directory_data(
+    #     selenium_get_html(
+    #         "https://www.twitch.tv/directory/game/Overwatch",
+    #         CHROME,
+    #         language="es"))
 
-    HTMLEN = selenium_get_html(
-        "https://www.twitch.tv/directory/game/Overwatch",
-        CHROME,
-        language="en")
-    DATAEN = get_directory_data(HTMLEN)
+    # DATAEN = get_directory_data(
+    #     selenium_get_html(
+    #         "https://www.twitch.tv/directory/game/Overwatch",
+    #         CHROME,
+    #         language="en"))
 
-    # Scrap
+    USERX = get_user_data(
+        selenium_get_html("https://www.twitch.tv/dafran", CHROME))
 
-    with open("overwatch_es.json", "w") as f:
-        json.dump(DATAES, f)
+    # Scrap results
 
-    with open("overwatch_en.json", "w") as f:
-        json.dump(DATAEN, f)
+    # with open("overwatch_es.json", "w") as f:
+    #     json.dump(DATAES, f)
+
+    # with open("overwatch_en.json", "w") as f:
+    #     json.dump(DATAEN, f)
+
+    with open("userx.json", "w") as f:
+        json.dump(USERX, f)
 
     # The end
 
