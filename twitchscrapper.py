798cd789c0333e683bccf09aa33fb67c7ca60486
alvivar <andresalvivar@gmail.com>
2018-01-07 01:09:28 -0600

    The script handles his dependencies  + Better exception handling + Simples testing

--- twitchscrapper.py
+++ twitchscrapper.py
@@ -11,38 +11,56 @@ import time
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+HOME = os.path.normpath(  # The script directory + cxfreeze compatibility
+    os.path.dirname(
+        sys.executable if getattr(sys, 'frozen', False) else __file__))
+
+CONFIG = {}
+CONFIGJSON = os.path.join(HOME, "config.json")
+try:
+    CONFIG = json.load(open(CONFIGJSON, "r"))
+except (IOError, ValueError):
+    CONFIG = {"config": {"chrome_driver_path": "chromedriver.exe"}}
+    with open(CONFIGJSON, "w") as f:
+        json.dump(CONFIG, f)
 
 
-def get_twitch_html(url, chrome_driver, language=None, closechat=None):
+def get_twitch_html(url, language=None, closechat=False):
     """
-        Return the html source from the Twitch.tv url using Selenium and the
-        Chrome web driver.
+        Return the html source from the Twitch.tv url using Selenium and Chrome
+        web driver.
 
         If language is specified (es, en, fr, etc) it will click on the language
         menu and change it (works only on directory pages).
 
-        If close chat, it will click the close chat toggle.
+        If close chat is true, it will click the close chat toggle.
     """
 
-    driver = webdriver.Chrome(chrome_driver)
+    driver = webdriver.Chrome(
+        os.path.join(HOME, CONFIG["config"]["chrome_driver_path"]))
+
     driver.maximize_window()
     driver.get(url)
 
-    # Change the language by 'clicking' on the menu
-    if language:
+    try:
+        if language:  # 'Click' the menu
+            langmenu = "//div[contains(@class, 'language-select-menu')]"
+            driver.find_element_by_xpath(langmenu).click()
+            time.sleep(1)
 
-        langmenu = "//div[contains(@class, 'language-select-menu')]"
-        driver.find_element_by_xpath(langmenu).click()
-        time.sleep(1)
+            langcheck = f"//div[@class='tw-checkbox' and @data-language-code='{language}']"
+            driver.find_element_by_xpath(langcheck).click()
+            time.sleep(1)
 
-        langcheck = f"//div[@class='tw-checkbox' and @data-language-code='{language}']"
-        driver.find_element_by_xpath(langcheck).click()
-        time.sleep(1)
-
-    if closechat:
-        togglecol = "//button[contains(@data-a-target, 'right-column__toggle-collapse-btn')]"
-        driver.find_element_by_xpath(togglecol).click()
-        time.sleep(1)
+        if closechat:  # Click the collapse chat button
+            togglecol = "//button[contains(@data-a-target, 'right-column__toggle-collapse-btn')]"
+            driver.find_element_by_xpath(togglecol).click()
+            time.sleep(1)
+    except NoSuchElementException:
+        driver.quit()
+        return False
 
     html = driver.page_source
     driver.quit()
@@ -52,10 +70,10 @@ def get_twitch_html(url, chrome_driver, language=None, closechat=None):
 
 def get_href_handler(htmlsource, href):
     """
-        Return the last part of the first url found with the href parameter in
-        any url link of the html.
+        Return the last part of the first url found if the href parameter is in
+        the href form the url found.
 
-        'lolirotve' from the url https://twitter.com/lolirotve when href is
+        e.g. 'lolirotve' from the url https://twitter.com/lolirotve when href is
         'twitter.com/'
     """
 
@@ -68,48 +86,62 @@ def get_href_handler(htmlsource, href):
     return False
 
 
-def get_directory_data(htmlsource):
+def get_directory_data(url, language="en"):
     """
         Return a dictionary with the data for each stream in a Twitch game
         directory page, like https://www.twitch.tv/directory/game/Overwatch
     """
 
+    htmlsource = get_twitch_html(url, language=language)
+    if not htmlsource:
+        return False
+
     soup = BeautifulSoup(htmlsource, "html.parser")
-
     data = {}
-    for html in soup.find_all("div", "stream-thumbnail"):
 
-        image = html.find("img", src=True)
-        image = image["src"] if image else False
+    try:
+        for html in soup.find_all("div", "stream-thumbnail"):
 
-        title = html.find("h3", class_="live-channel-card__title", title=True)
-        title = title["title"] if title else False
+            image = html.find("img", src=True)
+            image = image["src"] if image else False
 
-        viewers = html.find("span", class_="tw-ellipsis")
-        viewers = viewers.text.split(" ")[0] if viewers else False
+            title = html.find(
+                "h3", class_="live-channel-card__title", title=True)
+            title = title["title"] if title else False
 
-        user = html.find("a", class_="live-channel-card__videos", href=True)
-        user = user["href"].replace(
-            "/", " ").strip().split(" ")[0] if user else False
+            viewers = html.find("span", class_="tw-ellipsis")
+            viewers = viewers.text.split(" ")[0] if viewers else False
 
-        data[user] = {
-            "image": image,
-            "title": title,
-            "viewers": viewers,
-            "time": time.time()
-        }
+            user = html.find(
+                "a", class_="live-channel-card__videos", href=True)
+            user = user["href"].replace(
+                "/", " ").strip().split(" ")[0] if user else False
+
+            data[user] = {
+                "image": image,
+                "title": title,
+                "viewers": viewers,
+                "time": time.time()
+            }
+    except AttributeError:
+        print(
+            f"AttributeError: get_directory_data({url}, language='{language}')"
+        )
 
     return data
 
 
-def get_user_data(htmlsource):
+def get_user_data(url):
     """
         Return a dictionary with the user data on a Twitch.tv streamer page html
-        source, like https://www.twitch.tv/chipshajen
+        source, e.g https://www.twitch.tv/chipshajen
     """
 
-    soup = BeautifulSoup(htmlsource, "html.parser")
+    htmlsource = get_twitch_html(url, closechat=True)
+    if not htmlsource:
+        return False
 
+    soup = BeautifulSoup(htmlsource, "html.parser")
     data = {}
 
     user = soup.find("a", class_="channel-header__user").find("h5")
@@ -121,23 +153,29 @@ def get_user_data(htmlsource):
     })
     status = status["title"] if status else False
 
-    viewers = soup.find("div", {
-        "class": "tw-stat",
-        "data-a-target": "channel-viewers-count"
-    }).find("span", {
-        "data-a-target": "tw-stat-value"
-    })
-    viewers = viewers.text if viewers else False
-    viewers = "".join([c for c in viewers if c.isdigit()])
+    try:
+        viewers = soup.find("div", {
+            "class": "tw-stat",
+            "data-a-target": "channel-viewers-count"
+        }).find("span", {
+            "data-a-target": "tw-stat-value"
+        })
+        viewers = viewers.text if viewers else False
+        viewers = "".join([c for c in viewers if c.isdigit()])
+    except AttributeError:
+        viewers = -1
 
-    total_views = soup.find("div", {
-        "class": "tw-stat",
-        "data-a-target": "total-views-count"
-    }).find("span", {
-        "data-a-target": "tw-stat-value"
-    })
-    total_views = total_views.text if total_views else False
-    total_views = "".join([c for c in total_views if c.isdigit()])
+    try:
+        total_views = soup.find("div", {
+            "class": "tw-stat",
+            "data-a-target": "total-views-count"
+        }).find("span", {
+            "data-a-target": "tw-stat-value"
+        })
+        total_views = total_views.text if total_views else False
+        total_views = "".join([c for c in total_views if c.isdigit()])
+    except AttributeError:
+        total_views = -1
 
     twitter = get_href_handler(htmlsource, "twitter.com/")
     instagram = get_href_handler(htmlsource, "instagram.com/")
@@ -145,29 +183,38 @@ def get_user_data(htmlsource):
     youtubechannel = get_href_handler(htmlsource, "youtube.com/channel/")
     discord = get_href_handler(htmlsource, "discord.gg/")
 
-    followers = soup.find("a", {
-        "data-a-target": "followers-channel-header-item"
-    }).find("div", {
-        "class": "channel-header__item-count"
-    }).find("span")
-    followers = followers.text if followers else False
-    followers = "".join([c for c in followers if c.isdigit()])
+    try:
+        followers = soup.find("a", {
+            "data-a-target": "followers-channel-header-item"
+        }).find("div", {
+            "class": "channel-header__item-count"
+        }).find("span")
+        followers = followers.text if followers else False
+        followers = "".join([c for c in followers if c.isdigit()])
+    except AttributeError:
+        followers = -1
 
-    following = soup.find("a", {
-        "data-a-target": "following-channel-header-item"
-    }).find("div", {
-        "class": "channel-header__item-count"
-    }).find("span")
-    following = following.text if following else False
-    following = "".join([c for c in following if c.isdigit()])
+    try:
+        following = soup.find("a", {
+            "data-a-target": "following-channel-header-item"
+        }).find("div", {
+            "class": "channel-header__item-count"
+        }).find("span")
+        following = following.text if following else False
+        following = "".join([c for c in following if c.isdigit()])
+    except AttributeError:
+        following = -1
 
-    videos = soup.find("a", {
-        "data-a-target": "videos-channel-header-item"
-    }).find("div", {
-        "class": "channel-header__item-count"
-    }).find("span")
-    videos = videos.text if videos else False
-    videos = "".join([c for c in videos if c.isdigit()])
+    try:
+        videos = soup.find("a", {
+            "data-a-target": "videos-channel-header-item"
+        }).find("div", {
+            "class": "channel-header__item-count"
+        }).find("span")
+        videos = videos.text if videos else False
+        videos = "".join([c for c in videos if c.isdigit()])
+    except AttributeError:
+        videos = -1
 
     data[user] = {
         "status": status,
@@ -190,60 +237,23 @@ def get_user_data(htmlsource):
 
 if __name__ == "__main__":
 
+    # Testing
+
     DELTA = time.time()
 
-    # The current dir should be the script home, cxfreeze compatibility
+    with open(os.path.join(HOME, "overwatch_en.json"), "w") as f:
+        json.dump(
+            get_directory_data(
+                "https://www.twitch.tv/directory/game/Overwatch",
+                language="en"), f)
 
-    CDIR = os.path.normpath(
-        os.path.dirname(
-            sys.executable if getattr(sys, 'frozen', False) else __file__))
-    os.chdir(CDIR)
+    with open(os.path.join(HOME, "overwatch_es.json"), "w") as f:
+        json.dump(
+            get_directory_data(
+                "https://www.twitch.tv/directory/game/Overwatch",
+                language="es"), f)
 
-    # Config
-
-    CONFIGJSON = "config.json"
-
-    try:
-        CONFIG = json.load(open(CONFIGJSON, "r"))
-    except (IOError, ValueError):
-        CONFIG = {"config": {"chrome_driver_path": "chromedriver.exe"}}
-        with open(CONFIGJSON, "w") as f:
-            json.dump(CONFIG, f)
-
-    # Driver
-
-    CHROME = os.path.normpath(
-        os.path.join(CDIR, CONFIG["config"]["chrome_driver_path"]))
-
-    # Data
-
-    DATAES = get_directory_data(
-        get_twitch_html(
-            "https://www.twitch.tv/directory/game/Overwatch",
-            CHROME,
-            language="es"))
-
-    DATAEN = get_directory_data(
-        get_twitch_html(
-            "https://www.twitch.tv/directory/game/Overwatch",
-            CHROME,
-            language="en"))
-
-    USERX = get_user_data(
-        get_twitch_html(
-            "https://www.twitch.tv/tytannia92", CHROME, closechat=True))
-
-    # Scrap results
-
-    with open("overwatch_es.json", "w") as f:
-        json.dump(DATAES, f)
-
-    with open("overwatch_en.json", "w") as f:
-        json.dump(DATAEN, f)
-
-    with open("userx.json", "w") as f:
-        json.dump(USERX, f)
-
-    # The end
+    with open(os.path.join(HOME, "userx.json"), "w") as f:
+        json.dump(get_user_data("https://www.twitch.tv/aimbotcalvin"), f)
 
     print(f"\nDone! ({round(time.time() - DELTA)}s)")
