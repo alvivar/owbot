fb96614fa6539e9e2c87c1d67af4ce7d74cef170
alvivar <andresalvivar@gmail.com>
2018-01-12 21:03:45 -0600

    argparse arguments added + repeat cycle + bot commands detection (q to quit)

--- owbot.py
+++ owbot.py
@@ -7,10 +7,12 @@
     @matnesis 2018/01/10
 """
 
+import argparse
 import json
 import os
 import shutil
 import sys
+import threading
 import time
 from urllib.request import urlopen
 
@@ -74,86 +76,160 @@ except (IOError, ValueError):
 if __name__ == "__main__":
 
     DELTA = time.time()
-    print("@overwatchbest")
+    print("OWBOT!")
 
-    # Prepare a tweet for the top player
+    # Command line args
 
-    DIRURL = "https://www.twitch.tv/directory/game/Overwatch"
-    DIRECTORY = get_directory_data(DIRURL, language="en")
-    print(f"Scrapped '{DIRURL}'")
+    PARSER = argparse.ArgumentParser(description='Overwatch + Twitch.tv Bot')
+    PARSER.add_argument(
+        "-s",
+        "--start",
+        help="start the scrapping + queue process",
+        action="store_true")
+    PARSER.add_argument(
+        "-d",
+        "--delay",
+        help=
+        "seconds to wait between complete cycles, 600s (10m) default, use 0 or less to not repeat at all",
+        default=10 * 60,
+        type=int)
+    PARSER.add_argument(
+        "-b",
+        "--ban",
+        help=
+        "seconds to wait before repeating an account again, 28800s (8h) default",
+        default=8 * 3600,
+        type=int)
+    ARGS = PARSER.parse_args()
 
-    if not DIRECTORY:
-        input(f"\nError scraping '{DIRURL}'")
-        sys.exit(1)
+    # TODO DANGEROUS code: All new options need to be here or they will be ignored
+    if not ARGS.start:
+        PARSER.print_usage()
+        PARSER.exit()
 
-    with open(
-            os.path.join(DATAPATH, f"directory({round(time.time())}).json"),
-            "w") as f:
-        json.dump(DIRECTORY, f)
+    # Thread to detect input commands and stop the repeat cycle
 
-    for user, dirdata in DIRECTORY.items():
+    REPEAT = True
 
-        # Registry setup
+    def bot_commands():
+        """
+            Input detection thread.
+        """
+        global REPEAT
+        while REPEAT:
+            text = input()
+            if text.strip().lower() == "q":  # Quit
+                REPEAT = False
 
-        CONFIG['promoted'] = CONFIG.get('promoted', {})
-        CONFIG['promoted'][user] = CONFIG['promoted'].get(
-            user, {
-                'count': 0,
-                'first_time': time.time(),
-                'last_time': 0
-            })
+    THREAD = threading.Thread(target=bot_commands)
+    THREAD.daemon = True
+    THREAD.start()
 
-        # Avoid spamming users
+    # Repeat cycle
 
-        ban = 12 * 3600
-        if time.time() - CONFIG['promoted'][user]['last_time'] < ban:
-            continue
+    WAIT = 0
+    COUNT = 1
+    while REPEAT:
 
-        # Data
+        # Prepare a tweet for the top player
 
-        url = f"https://www.twitch.tv/{user}"
-        userdata = get_user_data(url)
-        print(f"Scrapped '{url}'\n")
+        DIRURL = "https://www.twitch.tv/directory/game/Overwatch"
+        DIRECTORY = get_directory_data(DIRURL, language="en")
+        print(f"Scrapped '{DIRURL}'")
 
-        if not userdata:
-            input(f"\nError scraping '{url}'")
+        if not DIRECTORY:
+            input(f"\nError scraping '{DIRURL}'")
             sys.exit(1)
 
         with open(
-                os.path.join(DATAPATH, f"{user}({round(time.time())}).json"),
+                os.path.join(DATAPATH,
+                             f"directory({round(time.time())}).json"),
                 "w") as f:
-            json.dump(userdata, f)
+            json.dump(DIRECTORY, f)
 
-        status = userdata[user]['status']
-        status = status if len(status) < 200 else status[:200] + "[...]"
-        status = " ".join(status.split())
+        for user, dirdata in DIRECTORY.items():
 
-        twitter = " ".join([f"@{i}" for i in userdata[user]['twitter']])
-        twitter = f" ({twitter}) " if twitter else " "
+            # Registry setup
 
-        imageurl = dirdata['image']
-        imagefile = os.path.join(IMAGESPATH, os.path.basename(imageurl))
-        with urlopen(imageurl) as r, open(imagefile, 'wb') as f:
-            shutil.copyfileobj(r, f)
-            print(f"Downloaded '{imagefile}'")
+            CONFIG['promoted'] = CONFIG.get('promoted', {})
+            CONFIG['promoted'][user] = CONFIG['promoted'].get(
+                user, {
+                    'count': 0,
+                    'first_time': time.time(),
+                    'last_time': 0
+                })
 
-        # Queue tweet in Qbot
+            # Avoid spamming users
 
-        tweet = {'text': f"{status}{twitter}{url}", 'image': imagefile}
-        QBOT['messages'].append(tweet)
-        with open(QBOTJSON, "w") as f:
-            json.dump(QBOT, f)
-            print(f"Tweet queued on '{QBOTJSON}'")
+            if time.time() - CONFIG['promoted'][user]['last_time'] < ARGS.ban:
+                continue
 
-        # Register
+            # Data
 
-        CONFIG['promoted'][user]['count'] += 1
-        CONFIG['promoted'][user]['last_time'] += time.time()
-        with open(CONFIGJSON, "w") as f:
-            json.dump(CONFIG, f)
+            url = f"https://www.twitch.tv/{user}"
+            userdata = get_user_data(url)
+            print(f"Scrapped '{url}'\n")
 
-        # Just once
-        break
+            if not userdata:
+                input(f"\nError scraping '{url}'")
+                sys.exit(1)
+
+            with open(
+                    os.path.join(DATAPATH,
+                                 f"{user}({round(time.time())}).json"),
+                    "w") as f:
+                json.dump(userdata, f)
+
+            status = userdata[user]['status']
+            status = status if len(status) < 200 else status[:200] + "[...]"
+            status = " ".join(status.split())
+
+            twitter = " ".join([f"@{i}" for i in userdata[user]['twitter']])
+            twitter = f" ({twitter}) " if twitter else " "
+
+            imageurl = dirdata['image']
+            imagefile = os.path.join(IMAGESPATH, os.path.basename(imageurl))
+            with urlopen(imageurl) as r, open(imagefile, 'wb') as f:
+                shutil.copyfileobj(r, f)
+                print(f"Downloaded '{imagefile}'")
+
+            # Queue tweet in Qbot
+
+            tweet = {'text': f"{status}{twitter}{url}", 'image': imagefile}
+            QBOT['messages'].append(tweet)
+            with open(QBOTJSON, "w") as f:
+                json.dump(QBOT, f)
+                print(f"Tweet queued on '{QBOTJSON}'")
+
+            # Register
+
+            CONFIG['promoted'][user]['count'] += 1
+            CONFIG['promoted'][user]['last_time'] += time.time()
+            with open(CONFIGJSON, "w") as f:
+                json.dump(CONFIG, f)
+
+            # Just once
+            break
+
+        # Repeat
+
+        REPEAT = False if ARGS.delay <= 0 else REPEAT
+        if REPEAT:
+            print()
+
+        while REPEAT and WAIT < ARGS.delay:
+            sys.stdout.write(
+                f"\r'q' and enter to quit ({ARGS.delay - WAIT}s): ")
+            sys.stdout.flush()
+
+            WAIT += 1
+            time.sleep(1)
+
+        if REPEAT:
+            WAIT = 0
+            COUNT += 1
+            print(f"\n\n#{COUNT}")
 
     # The end
-    input(f"\nDone! ({round(time.time() - DELTA)}s)")
+    print(f"\nDone! ({round(time.time() - DELTA)}s)")
+    time.sleep(2)
